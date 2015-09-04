36ea0339f12c812de80a88ad42e66173d2eda843
Ben Morimoto <bkmorimoto@gmail.com>
2015-09-03 20:23:34 -0400

    all tables have been migrated and schema created

--- db/migrate/20150904002114_create_session.rb
+++ db/migrate/20150904002114_create_session.rb
@@ -1,7 +1,7 @@
 class CreateSession < ActiveRecord::Migration
   def change
     create_table :sessions do |t|
-      t.referecnes :ramen
+      t.references :ramen
       t.references :user
       t.integer :rating
       t.text :description
