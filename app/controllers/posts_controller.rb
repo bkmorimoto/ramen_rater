d0dc84bba9bf97323fc9c47af12499a83866d615
Ben Morimoto <bkmorimoto@gmail.com>
2015-09-23 10:11:34 -0400

    added form to add post to index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,21 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+    post.user = User.find(1)
+    post.save
+
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:ramen_id, :rating, :description)
   end
 
 end
